e8311cb48bcc8db01c0956ae2049b59d1c4d04e1
Harshit Jain <harshit120299@gmail.com>
2021-08-01 11:30:00 +0530

    daily update

--- c++/Pythagorean-Triplet-Check.cpp
+++ c++/Pythagorean-Triplet-Check.cpp
@@ -5,18 +5,14 @@ using namespace std;
 
 int findMax(int a, int b, int c) {
     int max = INT_MIN;
-    if (a>b) {
-        (a>c) ? max = a : max = c;
-    } else {
-        (b>c) ? max = b : max = c;
-    }
+    (a>b) ? ((a>c) ? max = a : max = c) : ((b>c) ? max = b : max = c);
     return max;
 }
 
 bool checkPythagorean(int a, int b, int c) {
     bool isPythagorean = false;
     int max = findMax(a, b, c);
-    
+
     return isPythagorean;
 }
 
