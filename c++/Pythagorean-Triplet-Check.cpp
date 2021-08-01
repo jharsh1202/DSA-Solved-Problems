8a4bdbd8baec27ea3c073e70688a8c9bebe058ae
Harshit Jain <harshit120299@gmail.com>
2021-08-01 11:00:00 +0530

    daily update

---
+++ c++/Pythagorean-Triplet-Check.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+typedef long long ll;
+using namespace std;
+
+bool checkPythagorean(int a, int b, int c) {
+    bool isPythagorean = false;
+    
+    return isPythagorean;
+}
+
+int main() {
+    cout << checkPythagorean(2, 4, 5);
+}
