9a1b48ecdfa9d9c9c2190d8dbdae27c859ff153e
Harshit Jain <harshit120299@gmail.com>
2021-08-14 00:00:00 +0530

    Automated Commit

--- c++/Longest-Arithmetic-Array.cpp
+++ c++/Longest-Arithmetic-Array.cpp
@@ -2,17 +2,25 @@
 #include <vector>
 using namespace std;
 
-int main() {
-    vector<int> v={1,2,35,36,37,38,39,3246,3,46,34,14,12,10,6,4,2,0};
+int longestArithmeticArrayLength(vector<int> v) {
     int maxcount = 0, count = 0;
-    for (int i = 0; i < v.size()-2; i++) {
-        if(v[i]-v[i+1]==v[i+1]-v[i+2]) {
-            ++count;
-            if(count>maxcount) maxcount=count;
-        } else 
-            count=0;
+    int prevDiff=v[0]-v[1];count=2;maxcount=2;
+    for (int i = 1; i < v.size()-1; i++) {
+        if(v[i]-v[i+1]==prevDiff) {count++; if(count>maxcount)maxcount=count;}
+        else {prevDiff=v[i]-v[i+1];count=2;}
     }
-    if(maxcount>0) maxcount+=2; 
-    cout<<maxcount;
+    return maxcount;
+}
+
+int main() {
+    long int t;
+    cin>>t;
+    for(long int j=1; j<=t; j++) {
+        int n; cin>>n; vector<int> vec;
+        for(int i=0; i<n; i++) {
+            int v; cin>>v; vec.push_back(v);
+        }
+        cout<<"case #"<<j<<": "<<longestArithmeticArrayLength(vec)<<endl;
+    }
     return 0;
 }
